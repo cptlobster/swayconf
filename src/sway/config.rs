de464e22e2f2b83089d4d0273fac23dd3c0c8800
Dustin Thomas <stdio@cptlobster.dev>
2025-01-10 16:45:07 -0600

    refactor WritableConfig functions into a trait

--- src/sway/config.rs
+++ src/sway/config.rs
@@ -20,6 +20,12 @@ use std::fs::File;
 use std::io::{Write, Result};
 use std::path::PathBuf;
 use homedir::my_home;
+use std::mem::discriminant;
+
+trait WritableConfig {
+    fn write(&self) -> Result<usize>;
+    fn strip_comments(&self) -> Self;
+}
 
 /// A single configuration file
 #[derive(Debug, PartialEq, Eq, Clone)]
@@ -47,10 +53,17 @@ impl ConfigFile {
         new_cmds.append(&mut other_cmds);
         ConfigFile::new(self.path, new_cmds)
     }
+}
 
+impl WritableConfig for ConfigFile {
     fn write(self) -> Result<usize> {
         File::open(self.path.clone())
             .and_then(|mut f| {f.write(self.to_string().as_bytes())})
+    }
+
+    fn strip_comments(&self) -> ConfigFile {
+        let filtered = self.commands.iter().filter(|c| discriminant(c) != discriminant(&&Config::Comment(String::new()))).collect();
+        ConfigFile::new(self.path.clone(), filtered)
     }
 }
 
@@ -68,10 +81,26 @@ pub struct ConfigGroup {
 }
 
 impl ConfigGroup {
+    fn new(self, files: Vec<ConfigFile>) -> ConfigGroup {
+        ConfigGroup { files }
+    }
+
+    fn empty() -> ConfigGroup {
+        ConfigGroup { files: vec![] }
+    }
+
+    fn push(self, other: ConfigFile) -> ConfigGroup {
+        let mut files: Vec<ConfigFile> = self.files;
+        files.push(other);
+        ConfigGroup { files }
+    }
+
     fn flatten(self) -> ConfigFile {
         self.files.iter().cloned().reduce(|left, right| left.concat(right)).unwrap()
     }
+}
 
+impl WritableConfig for ConfigGroup {
     fn write(self) -> Result<usize> {
         let mut result: Result<usize> = Ok(0);
         for file in self.files {
@@ -81,6 +110,11 @@ impl ConfigGroup {
             }
         }
         result
+    }
+
+    fn strip_comments(&self) -> ConfigGroup {
+        let files = self.files.iter().map(|f| f.strip_comments()).collect();
+        ConfigGroup{ files }
     }
 }
 
@@ -112,5 +146,27 @@ mod tests {
 
         let file = ConfigFile::default(commands);
         assert_eq!(expected, file.to_string());
+    }
+
+    fn test_config_to_string() {
+        let commands: Vec<Config> = vec![
+            Config::ExecAlways("/bin/echo \"this is a command!\"".to_string()),
+            Config::Bindsym{flags: vec![], keys: vec!["Mod4".to_string(), "Space".to_string()], command: Box::new(Runtime::Exec("/bin/bash".to_string()))},
+            Config::Comment("move the currently focused window around".to_string()),
+            Config::Bindsym{flags: vec![], keys: vec!["Mod4".to_string(), "W".to_string()], command: Box::new(Runtime::Move(SubMove::Directional{direction: Directional::Up, px: None}))},
+            Config::Bindsym{flags: vec![], keys: vec!["Mod4".to_string(), "S".to_string()], command: Box::new(Runtime::Move(SubMove::Directional{direction: Directional::Down, px: None}))},
+            Config::Bindsym{flags: vec![], keys: vec!["Mod4".to_string(), "A".to_string()], command: Box::new(Runtime::Move(SubMove::Directional{direction: Directional::Left, px: None}))},
+            Config::Bindsym{flags: vec![], keys: vec!["Mod4".to_string(), "D".to_string()], command: Box::new(Runtime::Move(SubMove::Directional{direction: Directional::Right, px: None}))},
+        ];
+
+        let expected: String = "exec_always /bin/echo \"this is a command!\"\n\
+        bindsym Mod4+Space exec /bin/bash\n\
+        bindsym Mod4+W move up\n\
+        bindsym Mod4+S move down\n\
+        bindsym Mod4+A move left\n\
+        bindsym Mod4+D move right".to_string();
+
+        let file = ConfigFile::default(commands);
+        assert_eq!(expected, file.strip_comments().to_string());
     }
 }
